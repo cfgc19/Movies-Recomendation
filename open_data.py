093ac58046e6ade6f2a8484f17d500911e151944
cfgc19 <cfilipa.19@gmail.com>
2017-11-25 00:41:49 +0000

    simplifiquei a funcao open_Data

--- open_data.py
+++ open_data.py
@@ -1,12 +1,16 @@
 import csv
 import os
 products = {}
-with open('Dataset_SNAP.csv', 'r', encoding='utf-8') as movie_file:
+a=0;
+with open('Dataset_SNAP_with_movies.txt', 'r', encoding='utf-8') as movie_file:
     reader = csv.reader(movie_file)
     for i in reader:
+        print(i)
         for element in i:
-            list = element.split(',')
-            for j in list:
-                print(j)
-            print('another one')
+            print(element)
+            #list = element.split(',')
+            #print(list)
+            #for j in list:
+                # print(j)
+        print('another one')
 
